8605769cb8da6f44ccd2f55d0f0048ae42a22ce8
Nikita Kononenko <vmeansdev@gmail.com>
2026-02-10 13:51:29 +0100

    Initial Commit

---
+++ MoviesDBTests/MoviesDBTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesDBTests.swift
+//  MoviesDBTests
+//
+//  Created by Nikita Kononenko on 10.02.2026.
+//
+
+import Testing
+@testable import MoviesDB
+
+struct MoviesDBTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
