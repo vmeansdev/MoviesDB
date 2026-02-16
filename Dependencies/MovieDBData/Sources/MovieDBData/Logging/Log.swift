133868d57b3ef4d84ae4b576c904cc47496afff3
Nikita Kononenko <vmeansdev@gmail.com>
2026-02-16 19:55:22 +0100

    Remove force unwrap

--- Dependencies/MovieDBData/Sources/MovieDBData/Logging/Log.swift
+++ Dependencies/MovieDBData/Sources/MovieDBData/Logging/Log.swift
@@ -2,7 +2,7 @@ import Foundation
 import OSLog
 
 struct Log {
-    private static let subsystem = Bundle.module.bundleIdentifier!
+    private static let subsystem = Bundle.module.bundleIdentifier ?? "MovieDBData"
 
     static let network = Logger(subsystem: subsystem, category: "Network")
     static let storage = Logger(subsystem: subsystem, category: "LocalStorage")
